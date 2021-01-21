e195cfcf73bd4813850f18477eeb0e556325e36a
ocawthorne <ocawthorne@gmail.com>
2021-01-21 19:41:39 +0000

    map-and-reduce

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+   let newArr = []
+   let fn = element => {newArr.push(element*-1)}
+   sourceArray.forEach(fn)
+   return newArr
+}
+
+function mapToNoChange(sourceArray) {
+   return sourceArray
+   let newArr = []
+   let fn = element => {newArr.push(element)}
+   sourceArray.forEach(fn)
+   return newArr
+}
+
+function mapToDouble(sourceArray) {
+   let newArr = []
+   let fn = element => {newArr.push(element*2)}
+   sourceArray.forEach(fn)
+   return newArr
+}
+
+function mapToSquare(sourceArray) {
+   let newArr = []
+   let fn = element => {newArr.push(element**2)}
+   sourceArray.forEach(fn)
+   return newArr
+}
+ 
+function reduceToTotal(sourceArray, startingPoint = 0){
+   sourceArray.forEach(element => {startingPoint += element})
+   return startingPoint
+}
+
+function reduceToAllTrue(sourceArray) {
+   let boolean = true
+   sourceArray.forEach(element => {boolean = boolean && !!element})
+   return boolean
+}
+
+function reduceToAnyTrue(sourceArray) {
+   let boolean = false
+   sourceArray.forEach(element => {boolean += !!element})
+   return !!boolean
+}
